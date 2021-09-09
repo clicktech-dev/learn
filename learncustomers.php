723f5fc91b65528a5e034deb9703d3f84da5604b
Charlie <my155mail155@gmail.com>
2021-09-10 01:28:35 +0800

    LEARN CUSTOMERS
    We want to learn our customers.

---
+++ learncustomers.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Thanks <?php echo $_POST["name"]; ?>!<br>
+So you visit us because: <?php echo $_POST["reason of using our site"]; ?>
+
+We will only keep private that you are <?php echo $_POST["gender"]; ?><br>
+and also your email will not be used unless you approve at the time.
+
+</body>
+</html>
